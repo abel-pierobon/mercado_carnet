826a79533c7bd64294557d2146bc747e46623a57
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-08-29 17:35:01 -0300

    logos en footer

--- src/Call.js
+++ src/Call.js
@@ -4,14 +4,14 @@ function Call({ turno }) {
     const horaFormateada = turno.timestamp ? new Date(turno.timestamp.toDate()).toLocaleTimeString() : '';
 
     return (
-        <div key={turno.id} className="border border-black card shadow-md py-2 px-2 rounded-md llamados">
+        <div key={turno.id} className="border border-gray-800 border-x-2 border-y-2 card shadow-md py-2 px-2 rounded-md llamados">
             <div className="flex justify-between">
-                <h2 className="text-start font-black uppercase m-3 text-base md:text-lg 2xl:text-2xl">
+                <h2 className="text-start font-black uppercase m-1 text-base md:text-lg 2xl:text-2xl">
                 {turno.apellido} {turno.nombre}
                 </h2>
-                <h2 className="text-end font-black uppercase m-3  text-base md:text-lg 2xl:text-2xl">{turno.puesto}</h2>
+                <h2 className="text-end font-semibold uppercase m-1  text-base md:text-lg 2xl:text-2xl">{turno.puesto}</h2>
             </div>
-                <p className="hidden  md:text-start uppercase m-3">{horaFormateada}</p>
+                {/* <p className="hidden  md:text-start uppercase m-3">{horaFormateada}</p> */}
         </div>
     );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,29 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer className="flex flex-col bg-[#59a0e7] h-[10%] xl:h-[10%]  ">
-            <div className="flex justify-center items-center h-6 text-black font-bold font-[Maven Pro]  text-sm">
-                <p className='mt-4'>
+        <footer className="flex justify-around bg-gray-200 h-[10%] xl:h-[10%]  ">
+            <div className="flex justify-start ">
+                <img
+                    className=" hidden lg:flex justify-center items-center lg:w-16 lg:h-16 2xl:w-20 2xl:h-20  "
+                    src="https://cordoba.gob.ar/wp-content/uploads/2023/12/cropped-Logo-Color_Mesa-de-trabajo-1-80x80.png"
+                    alt="cropped-Logo-Color_Mesa-de-trabajo-1.png"
+                    title="cropped-Logo-Color_Mesa-de-trabajo-1.png"
+                ></img>
+            </div>
+            
+            <div className="flex justify-center items-center h-6 text-black font-bold font-[Maven Pro]  text-xs   lg:text-base">
+                <p className='mt-10 text-center' >
                     &copy; copyright 2024 -Desarrollado por Personal del CPC
                     Mercado de la Ciudad
                 </p>
+            </div>
+            <div className="hidden lg:flex justify-start  ">
+                <img
+                    src="https://cordoba.gob.ar/wp-content/uploads/2020/08/logo-celeste-txt.svg"
+                    classname="w-28 h-28"
+                    alt=""
+                    decoding="async"
+                ></img>
             </div>
         </footer>
     );
