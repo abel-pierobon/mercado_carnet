91ca318b23c61b3674477b82bf335f2ea7364491
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-08-31 13:25:05 -0300

    arreglos en tamaño de letras

--- src/Footer.js
+++ src/Footer.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer className="flex justify-around bg-gray-200 h-[10%] xl:h-[10%]  ">
+        <footer className="flex justify-around bg-gray-200 h-[5%] 2xl:h-[10%]  ">
             <div className="flex justify-start ">
                 <img
-                    className=" hidden lg:flex justify-center items-center lg:w-16 lg:h-16 2xl:w-20 2xl:h-20  "
+                    className=" hidden lg:flex justify-center items-center lg:w-12 lg:h-12 2xl:w-20 2xl:h-20  "
                     src="https://cordoba.gob.ar/wp-content/uploads/2023/12/cropped-Logo-Color_Mesa-de-trabajo-1-80x80.png"
                     alt="cropped-Logo-Color_Mesa-de-trabajo-1.png"
                     title="cropped-Logo-Color_Mesa-de-trabajo-1.png"
                 ></img>
             </div>
             
-            <div className="flex justify-center items-center h-6 text-black font-bold font-[Maven Pro]  text-xs   lg:text-base">
-                <p className='mt-10 text-center' >
+            <div className="flex justify-center items-center h-6 text-black font-bold font-[Maven Pro]  text-xs   2xl:text-base">
+                <p className=' text-center flex justify-center items-center' >
                     &copy; copyright 2024 -Desarrollado por Personal del CPC
                     Mercado de la Ciudad
                 </p>
--- src/Vista.js
+++ src/Vista.js
@@ -27,7 +27,7 @@ function Vista() {
         const q = query(
             llamadoCollection,
             orderBy('timestamp', 'desc'),
-            limit(5),
+            limit(6),
         );
 
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
