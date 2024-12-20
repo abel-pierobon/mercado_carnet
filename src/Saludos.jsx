9e6e3d86187cb4f0e5aaeb07c24e32ba09bf91d3
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-12-20 10:58:42 -0300

    mayusculas

--- src/Saludos.jsx
+++ src/Saludos.jsx
@@ -3,7 +3,7 @@ function Saludos() {
         <div>
             <div className="flex justify-center items-center ">
                 <p className="text-center font-bold text-animate text-4xl">
-                    Desde el CPC mercado de la ciudad les deseamos Felices
+                    Desde el CPC Mercado de la Ciudad les deseamos Felices
                     Fiestas y un feliz 2025
                 </p>
             </div>
